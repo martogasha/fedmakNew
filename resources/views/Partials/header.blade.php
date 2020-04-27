415a6c0115dd42204bf4c455702655e005563007
Max <you@example.com>
2020-04-27 07:13:23 +0300

    first commit

---
+++ resources/views/Partials/header.blade.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+<!-- Mirrored from light.pinsupreme.com/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 20 Apr 2020 15:54:52 GMT -->
+<head><title>Admin Dashboard HTML Template</title>
+    <meta charset="utf-8">
+    <meta content="ie=edge" http-equiv="x-ua-compatible">
+    <meta content="template language" name="keywords">
+    <meta content="Tamerlan Soziev" name="author">
+    <meta content="Admin dashboard html template" name="description">
+    <meta content="width=device-width,initial-scale=1" name="viewport">
+    <link href="favicon.png" rel="shortcut icon">
+    <link href="apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="http://fast.fonts.net/cssapi/487b73f1-c2d1-43db-8526-db577e4c822b.css" rel="stylesheet">
+    <link href="bower_components/select2/dist/css/select2.min.css" rel="stylesheet">
+    <link href="bower_components/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <link href="bower_components/dropzone/dist/dropzone.css" rel="stylesheet">
+    <link href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link href="bower_components/fullcalendar/dist/fullcalendar.min.css" rel="stylesheet">
+    <link href="bower_components/perfect-scrollbar/css/perfect-scrollbar.min.css" rel="stylesheet">
+    <link href="bower_components/slick-carousel/slick/slick.css" rel="stylesheet">
+    <link href="css/main5739.css?version=4.5.0" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body class="menu-position-side menu-side-left full-screen with-content-panel">
+<div class="all-wrapper with-side-panel solid-bg-all">
+    <div class="search-with-suggestions-w">
+        <div class="search-with-suggestions-modal">
+            <div class="element-search"><input class="search-suggest-input" placeholder="Start typing to search...">
+                <div class="close-search-suggestions"><i class="os-icon os-icon-x"></i></div>
+            </div>
+            <div class="search-suggestions-group">
+                <div class="ssg-header">
+                    <div class="ssg-icon">
+                        <div class="os-icon os-icon-box"></div>
+                    </div>
+                    <div class="ssg-name">Projects</div>
+                    <div class="ssg-info">24 Total</div>
+                </div>
+                <div class="ssg-content">
+                    <div class="ssg-items ssg-items-boxed"><a class="ssg-item" href="users_profile_big.html">
+                            <div class="item-media" style="background-image: url(img/company6.png)"></div>
+                            <div class="item-name">Integ<span>ration</span> with API</div>
+                        </a><a class="ssg-item" href="users_profile_big.html">
+                            <div class="item-media" style="background-image: url(img/company7.png)"></div>
+                            <div class="item-name">Deve<span>lopm</span>ent Project</div>
+                        </a></div>
+                </div>
+            </div>
+            <div class="search-suggestions-group">
+                <div class="ssg-header">
+                    <div class="ssg-icon">
+                        <div class="os-icon os-icon-users"></div>
+                    </div>
+                    <div class="ssg-name">Customers</div>
+                    <div class="ssg-info">12 Total</div>
+                </div>
+                <div class="ssg-content">
+                    <div class="ssg-items ssg-items-list"><a class="ssg-item" href="users_profile_big.html">
+                            <div class="item-media" style="background-image: url(img/avatar1.jpg)"></div>
+                            <div class="item-name">John Ma<span>yer</span>s</div>
+                        </a><a class="ssg-item" href="users_profile_big.html">
+                            <div class="item-media" style="background-image: url(img/avatar2.jpg)"></div>
+                            <div class="item-name">Th<span>omas</span> Mullier</div>
+                        </a><a class="ssg-item" href="users_profile_big.html">
+                            <div class="item-media" style="background-image: url(img/avatar3.jpg)"></div>
+                            <div class="item-name">Kim C<span>olli</span>ns</div>
+                        </a></div>
+                </div>
+            </div>
+            <div class="search-suggestions-group">
+                <div class="ssg-header">
+                    <div class="ssg-icon">
+                        <div class="os-icon os-icon-folder"></div>
+                    </div>
+                    <div class="ssg-name">Files</div>
+                    <div class="ssg-info">17 Total</div>
+                </div>
+                <div class="ssg-content">
+                    <div class="ssg-items ssg-items-blocks"><a class="ssg-item" href="#">
+                            <div class="item-icon"><i class="os-icon os-icon-file-text"></i></div>
+                            <div class="item-name">Work<span>Not</span>e.txt</div>
+                        </a><a class="ssg-item" href="#">
+                            <div class="item-icon"><i class="os-icon os-icon-film"></i></div>
+                            <div class="item-name">V<span>ideo</span>.avi</div>
+                        </a><a class="ssg-item" href="#">
+                            <div class="item-icon"><i class="os-icon os-icon-database"></i></div>
+                            <div class="item-name">User<span>Tabl</span>e.sql</div>
+                        </a><a class="ssg-item" href="#">
+                            <div class="item-icon"><i class="os-icon os-icon-image"></i></div>
+                            <div class="item-name">wed<span>din</span>g.jpg</div>
+                        </a></div>
+                    <div class="ssg-nothing-found">
+                        <div class="icon-w"><i class="os-icon os-icon-eye-off"></i></div>
+                        <span>No files were found. Try changing your query...</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="layout-w">
+        <div class="menu-mobile menu-activated-on-click color-scheme-dark">
+            <div class="mm-logo-buttons-w"><a class="mm-logo" href="{{url('/')}}"><img src="img/logo.png"><span>Fedmak Agency</span></a>
+                    <div class="content-panel-open">
+                        <div class="os-icon os-icon-grid-circles"></div>
+                    </div>
+            </div>
+        </div>
+        <div class="content-w">
+            <div class="top-bar color-scheme-transparent">
+                <div class="menu-w color-scheme-light color-style-transparent menu-position-side menu-side-left menu-layout-compact sub-menu-style-over sub-menu-color-bright selected-menu-color-light menu-activated-on-hover menu-has-selected-link">
+                    <div class="logo-w"><a class="logo" href="{{url('/')}}">
+                            <div class="logo-element"></div>
+                            <div class="logo-label">Fedmak Agency</div>
+                        </a></div>
+
+                </div>
+
+                <div class="top-menu-controls">
+                    <div class="element-search autosuggest-search-activator"><input
+                            placeholder="Start typing to search..."></div>
+                    <div class="messages-notifications os-dropdown-trigger os-dropdown-position-left"><i
+                            class="os-icon os-icon-mail-14"></i>
+                        <div class="new-messages-count">12</div>
+                        <div class="os-dropdown light message-list">
+                            <ul>
+                                <li><a href="#">
+                                        <div class="user-avatar-w"><img alt="" src="img/avatar1.jpg"></div>
+                                        <div class="message-content"><h6 class="message-from">John Mayers</h6><h6
+                                                class="message-title">Account Update</h6></div>
+                                    </a></li>
+                                <li><a href="#">
+                                        <div class="user-avatar-w"><img alt="" src="img/avatar2.jpg"></div>
+                                        <div class="message-content"><h6 class="message-from">Phil Jones</h6><h6
+                                                class="message-title">Secutiry Updates</h6></div>
+                                    </a></li>
+                                <li><a href="#">
+                                        <div class="user-avatar-w"><img alt="" src="img/avatar3.jpg"></div>
+                                        <div class="message-content"><h6 class="message-from">Bekky Simpson</h6><h6
+                                                class="message-title">Vacation Rentals</h6></div>
+                                    </a></li>
+                                <li><a href="#">
+                                        <div class="user-avatar-w"><img alt="" src="img/avatar4.jpg"></div>
+                                        <div class="message-content"><h6 class="message-from">Alice Priskon</h6><h6
+                                                class="message-title">Payment Confirmation</h6></div>
+                                    </a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="logged-user-w">
+                        <div class="logged-user-i">
+                            <div class="avatar-w"><img alt="" src="img/avatar1.jpg"></div>
+                            <div class="logged-user-menu color-style-bright">
+                                <div class="logged-user-avatar-info">
+                                    <div class="avatar-w"><img alt="" src="img/avatar1.jpg"></div>
+                                    <div class="logged-user-info-w">
+                                        <div class="logged-user-name">Maria Gomez</div>
+                                        <div class="logged-user-role">Administrator</div>
+                                    </div>
+                                </div>
+                                <div class="bg-icon"><i class="os-icon os-icon-wallet-loaded"></i></div>
+                                <ul>
+                                    <li><a href="users_profile_big.html"><i
+                                                class="os-icon os-icon-user-male-circle2"></i><span>Profile Details</span></a>
+                                    </li>
+
+                                    <li><a href="#"><i class="os-icon os-icon-signs-11"></i><span>Logout</span></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <ul class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item"><a href="index.html">Products</a></li>
+                <li class="breadcrumb-item"><span>Laptop with retina screen</span></li>
+            </ul>
+            <div class="content-panel-toggler"><i class="os-icon os-icon-grid-squares-22"></i><span>Sidebar</span></div>
+            <div class="content-i">
+                <div class="content-box">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="element-wrapper">
+                                <div class="element-actions">
+                                    <form class="form-inline justify-content-sm-end"><select
+                                            class="form-control form-control-sm">
+                                            <option value="Pending">Today</option>
+                                            <option value="Active">Last Week</option>
+                                            <option value="Cancelled">Last 30 Days</option>
+                                        </select></form>
+                                </div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,215 @@
+@include('Partials.header')
+
+                                <h6 class="element-header">Dashboard</h6>
+                                <div class="element-content">
+                                    <div class="row">
+                                        <div class="col-sm-4 col-xxxl-3"><a class="element-box el-tablo" href="#">
+                                                <div class="label">Properties</div>
+                                                <div class="value">57</div>
+                                            </a>
+                                        </div>
+
+                                        <div class="col-sm-4 col-xxxl-3"><a class="element-box el-tablo" href="#">
+                                                <div class="label">Tenants</div>
+                                                <div class="value">557</div>
+                                            </a>
+                                        </div>
+                                        <div class="col-sm-4 col-xxxl-3"><a class="element-box el-tablo" href="#">
+                                                <div class="label">Tenant Details</div>
+                                                <i class="fa fa-book fa-3x" aria-hidden="true">
+                                                </i>
+                                            </a>
+                                        </div>
+                                        <div class="col-sm-4 col-xxxl-3"><a class="element-box el-tablo" href="#">
+                                                <div class="label">Payment & Billing</div>
+                                                <i class="fa fa-dollar fa-3x" aria-hidden="true">
+                                                </i>
+                                            </a>
+                                        </div>
+                                        <div class="col-sm-4 col-xxxl-3"><a class="element-box el-tablo" href="#">
+                                                <div class="label">Messages</div>
+                                                <i class="fa fa-envelope fa-3x" aria-hidden="true">
+                                                    <div class="trending trending-up-basic"><span>New Message</span></div>
+                                                </i>
+
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="element-wrapper"><h6 class="element-header">General Monthly Review</h6>
+                                <div class="element-box-tp">
+                                    <div class="controls-above-table">
+                                        <div class="row">
+                                            <div class="col-sm-6"><a class="btn btn-sm btn-secondary" href="#">Download
+                                                    CSV</a><a class="btn btn-sm btn-secondary" href="#">Archive</a><a
+                                                    class="btn btn-sm btn-danger" href="#">Delete</a></div>
+                                            <div class="col-sm-6">
+                                                <form class="form-inline justify-content-sm-end"><input
+                                                        class="form-control form-control-sm rounded bright"
+                                                        placeholder="Search"><select
+                                                        class="form-control form-control-sm rounded bright">
+                                                        <option selected="selected" value="">Select Status</option>
+                                                        <option value="Pending">Completed</option>
+                                                        <option value="Active">Uncompleted</option>
+                                                    </select></form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered table-lg table-v2 table-striped">
+                                            <thead>
+                                            <tr>
+                                                <th>Property Name</th>
+                                                <th>No. of Houses</th>
+                                                <th>Paid Houses</th>
+                                                <th>Houses with Balance</th>
+                                                <th>Status</th>
+                                                <th>Actions</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+                                                <td>Kikuyu Flats</td>
+                                                <td>50</td>
+                                                <td class="text-right">25</td>
+                                                <td class="text-right">25</td>
+                                                <td class="text-center">
+                                                    <div class="status-pill green" data-title="Complete"
+                                                         data-toggle="tooltip"></div><i>Completed</i>
+                                                </td>
+                                                <td class="row-actions"><a href="{{url('tables')}}"><i class="os-icon os-icon-ui-49"></i>View</a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>Limuru Flats</td>
+                                                <td>1500</td>
+                                                <td class="text-right">1000</td>
+                                                <td class="text-right">500</td>
+                                                <td class="text-center">
+                                                    <div class="status-pill red" data-title="Cancelled"
+                                                         data-toggle="tooltip"></div><i>Uncompleted</i>
+                                                </td>
+                                                <td class="row-actions"><a href="{{url('tables')}}"><i class="os-icon os-icon-ui-49"></i>View</a>
+                                                </td>
+                                            </tr>
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="controls-below-table">
+                                        <div class="table-records-info">Showing records 1 - 5</div>
+                                        <div class="table-records-pages">
+                                            <ul>
+                                                <li><a href="#">Previous</a></li>
+                                                <li><a class="current" href="#">1</a></li>
+                                                <li><a href="#">2</a></li>
+                                                <li><a href="#">3</a></li>
+                                                <li><a href="#">4</a></li>
+                                                <li><a href="#">Next</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="content-panel">
+                    <div class="content-panel-close"><i class="os-icon os-icon-close"></i></div>
+                    <div class="element-wrapper"><h6 class="element-header">Quick Links</h6>
+                        <div class="element-box-tp">
+                            <div class="el-buttons-list full-width"><a class="btn btn-white btn-sm" href="#"><i
+                                        class="os-icon os-icon-delivery-box-2"></i><span>Create New Property</span></a><a
+                                    class="btn btn-white btn-sm" href="#"><i class="os-icon os-icon-window-content"></i><span>Recent Payments</span></a><a
+                                    class="btn btn-white btn-sm" href="#"><i
+                                        class="os-icon os-icon-wallet-loaded"></i><span>Store Settings</span></a></div>
+                        </div>
+                    </div>
+                    <div class="element-wrapper"><h6 class="element-header">Property Owners</h6>
+                        <div class="element-box-tp">
+                            <div class="profile-tile"><a class="profile-tile-box" href="users_profile_small.html">
+                                    <div class="pt-avatar-w"><img alt="" src="img/defaultAvatar.png"></div>
+                                    <div class="pt-user-name">John Mayers</div>
+                                </a>
+                                <div class="profile-tile-meta">
+                                    <ul>
+                                        <li>Property:<strong>Kikuyu Flats</strong></li>
+                                        <li>No. of Houses:<strong><a href="apps_support_index.html">12</a></strong></li>
+                                        <li>Location:<strong>Kikuyu</strong></li>
+                                    </ul>
+                                    <div class="pt-btn"><a class="btn btn-success btn-sm" href="apps_full_chat.html">Administrator</a></div>
+                                </div>
+                            </div>
+                            <div class="profile-tile"><a class="profile-tile-box" href="users_profile_small.html">
+                                    <div class="pt-avatar-w"><img alt="" src="img/avatar3.jpg"></div>
+                                    <div class="pt-user-name">Ben Gossman</div>
+                                </a>
+                                <div class="profile-tile-meta">
+                                    <ul>
+                                        <li>Property:<strong>Limuru Flats</strong></li>
+                                        <li>No. of Houses:<strong><a href="apps_support_index.html">9</a></strong></li>
+                                        <li>Location:<strong>Limuru</strong></li>
+                                    </ul>
+                                    <div class="pt-btn"><a class="btn btn-secondary btn-sm" href="apps_full_chat.html">Owner</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="display-type"></div>
+</div>
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/popper.js/dist/umd/popper.min.js"></script>
+<script src="bower_components/moment/moment.js"></script>
+<script src="bower_components/chart.js/dist/Chart.min.js"></script>
+<script src="bower_components/select2/dist/js/select2.full.min.js"></script>
+<script src="bower_components/jquery-bar-rating/dist/jquery.barrating.min.js"></script>
+<script src="bower_components/ckeditor/ckeditor.js"></script>
+<script src="bower_components/bootstrap-validator/dist/validator.min.js"></script>
+<script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="bower_components/ion.rangeSlider/js/ion.rangeSlider.min.js"></script>
+<script src="bower_components/dropzone/dist/dropzone.js"></script>
+<script src="bower_components/editable-table/mindmup-editabletable.js"></script>
+<script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
+<script src="bower_components/perfect-scrollbar/js/perfect-scrollbar.jquery.min.js"></script>
+<script src="bower_components/tether/dist/js/tether.min.js"></script>
+<script src="bower_components/slick-carousel/slick/slick.min.js"></script>
+<script src="bower_components/bootstrap/js/dist/util.js"></script>
+<script src="bower_components/bootstrap/js/dist/alert.js"></script>
+<script src="bower_components/bootstrap/js/dist/button.js"></script>
+<script src="bower_components/bootstrap/js/dist/carousel.js"></script>
+<script src="bower_components/bootstrap/js/dist/collapse.js"></script>
+<script src="bower_components/bootstrap/js/dist/dropdown.js"></script>
+<script src="bower_components/bootstrap/js/dist/modal.js"></script>
+<script src="bower_components/bootstrap/js/dist/tab.js"></script>
+<script src="bower_components/bootstrap/js/dist/tooltip.js"></script>
+<script src="bower_components/bootstrap/js/dist/popover.js"></script>
+<script src="js/demo_customizer5739.js?version=4.5.0"></script>
+<script src="js/main5739.js?version=4.5.0"></script>
+<script>(function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-42863888-9', 'auto');
+    ga('send', 'pageview');</script>
+</body>
+<!-- Mirrored from light.pinsupreme.com/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 20 Apr 2020 15:55:47 GMT -->
+</html>
