6b78b823034e4b63a5286b70b86361c66ac178df
max <m.gachanga97@gmail.com>
2020-06-04 21:36:35 +0300

    finalize

---
+++ vendor/spatie/laravel-tail/src/TailServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Spatie\Tail;
+
+use Illuminate\Support\ServiceProvider;
+
+class TailServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/tail.php' => config_path('tail.php'),
+        ], 'tail-config');
+    }
+
+    public function register()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                TailCommand::class,
+            ]);
+        }
+
+        $this->mergeConfigFrom(__DIR__.'/../config/tail.php', 'tail');
+    }
+
+    public function provides()
+    {
+        return [
+            TailCommand::class,
+        ];
+    }
+}
