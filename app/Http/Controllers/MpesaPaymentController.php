edea3d1b77910ab4180a0b4dfea17f17d67b8cf5
max <m.gachanga97@gmail.com>
2020-06-05 16:38:37 +0300

    p

--- app/Http/Controllers/MpesaPaymentController.php
+++ app/Http/Controllers/MpesaPaymentController.php
@@ -71,13 +71,51 @@ class MpesaPaymentController extends Controller
     public function getValidation(){
 
     }
- public function simulate(){
-     $mpesa= new Mpesa();
-     $c2bTransaction= $mpesa->c2b(601426, 'CustomerPayBillOnline', 40000, 254790268795, 'A1' );
+ public function simulate()
+ {
+     $mpesa = new Mpesa();
+     $c2bTransaction = $mpesa->c2b(601426, 'CustomerPayBillOnline', 40000, 254790268795, 'A1');
      var_dump($c2bTransaction);
 
  }
  public function lipaNaMpesa(){
+     $token = Mpesa::generateSandBoxToken();
+
+     $url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+     $password = base64_encode('174379'.'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'.date('YmdGis'));
+
+     $curl = curl_init();
+     curl_setopt($curl, CURLOPT_URL, $url);
+     curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$token)); //setting custom header
+
+
+     $curl_post_data = array(
+         //Fill in the request parameters with valid values
+         'BusinessShortCode' => '174379 ',
+         'Password' =>  $password,
+         'Timestamp' => date('YmdGis'),
+         'TransactionType' => 'CustomerPayBillOnline',
+         'Amount"' => ' 40000',
+         'PartyA' => '254769722803 ',
+         'PartyB' => '174379 ',
+         'PhoneNumber' => ' 254769722803',
+         'CallBackURL' => 'https://kibe.braxlan.com/getPayment',
+         'AccountReference' => ' OK',
+         'TransactionDesc' => 'Ok '
+     );
+
+     $data_string = json_encode($curl_post_data);
+
+     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+     curl_setopt($curl, CURLOPT_POST, true);
+     curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+     $curl_response = curl_exec($curl);
+     print_r($curl_response);
+
+     echo $curl_response;
+ }
+ public function stkPush(){
      $mpesa= new \Safaricom\Mpesa\Mpesa();
      $BusinessShortCode = 174379;
      $LipaNaMpesaPasskey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
